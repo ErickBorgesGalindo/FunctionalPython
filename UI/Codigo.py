4f788907a7b1d94d1d2a0bde6cdb8073a5515609
Alecs <erick.borges@outlook.es>
2024-01-30 23:00:00 -0600

    fix: Modificiaciones a Codigo.py, unicamente descargar videos, descargar playlist y conversor a mp3, se agrega opcion para eliminar videos

--- UI/Codigo.py
+++ UI/Codigo.py
@@ -28,48 +28,29 @@ def video_playlist():
         print("Video descargado como ", filename)
     print("Playlist descargada")
 
-    
-#----------- Descargar audios ------------
-def audio():
-    import os
-    link = videos.get()
-    yt = YouTube(link)
-    yt = yt.streams.get_audio_only()
-    yt.download()
-    print("Descarga con éxito")
-
-def audio_playlist():
-    import os
-    print("Comenzando")
-    #Escribir playlist a descargar
-    link = videos.get()
-    playlist=Playlist(link)
-    for video in playlist.videos:
-        url=video.watch_url
-        yt = pytube.YouTube(url)
-        stream = yt.streams.get_audio_only()
-        filename = stream.download()
-        print("Audio descargado como ", filename)
-    print("Playlist descargada")
-
 #----------- Convertirdores ------------
 def mp4():
-    from pathlib import Path
-    carpeta= Path('converter')
-    for c in list (carpeta.iterdir()):
-        if c.suffix == ".mp4":
-            nueva_extension = c.with_suffix(".mp3")
-            c.rename(nueva_extension)
-    print("Extensión cambiada")
+    from tkinter import Tk
+    import tkinter.filedialog as tkf
+    import moviepy.editor as editor
+    import os
 
-def m4a():
-    from pathlib import Path
-    carpeta= Path('converter')
-    for c in list (carpeta.iterdir()):
-        if c.suffix == ".m4a":
-            nueva_extension = c.with_suffix(".mp3")
-            c.rename(nueva_extension)
-    print("Extensión cambiada")
+    root = Tk()
+    root.withdraw()
+
+    rutas = tkf.askopenfilenames(filetypes=[('Archivos mp4', '*.mp4')])
+    for ruta in rutas:
+        nombre = os.path.basename(ruta)
+        video_clip = editor.VideoFileClip(ruta)
+        video_clip.audio.write_audiofile(nombre.replace('.mp4','.mp3'))
+import os
+
+#----------- Automatización ------------
+def eliminar_videos():
+    for archivo in os.listdir():
+        if archivo.endswith(".mp4"):
+            os.remove(os.path.join(archivo))
+
 
 def popup():
     Messagebox.showinfo("Holi","Yo hice esto")
@@ -78,12 +59,12 @@ root = Tk()
 root.config(bd=15)
 root.title("Multi-Media-Tools")
 
-img = Image.open("alex.png")
-resized_img = img.resize((200, 200), Image.ANTIALIAS)
-photo = ImageTk.PhotoImage(resized_img)
+#img = Image.open("alex.png")
+#resized_img = img.resize((200, 200), Image.ANTIALIAS)
+#photo = ImageTk.PhotoImage(resized_img)
 #image = PhotoImage(file="batma.png")
-foto = Label(root, image=photo, bd=0)
-foto.grid(row=0, column=0)
+#foto = Label(root, image=photo, bd=0)
+#foto.grid(row=0, column=0)
 
 menubar = Menu(root)
 root.config(menu=menubar)
@@ -103,13 +84,9 @@ boton = Button(root, text="Descargar Video", command = video)
 boton.grid(row=3, column=0)
 boton2= Button(root, text="Playlist de Videos", command = video_playlist)
 boton2.grid(row=4, column=0)
-boton3 = Button(root, text="Descargar Audio", command = audio)
+boton3 = Button(root, text="De MP4 - MP3 ", command = mp4)
 boton3.grid(row=5, column=0)
-boton4= Button(root, text="Playlist de Audios", command = audio_playlist)
+boton4 = Button(root, text="Eliminar videos ", command = eliminar_videos)
 boton4.grid(row=6, column=0)
-boton5= Button(root, text="De MP4 - MP3 ", command = mp4)
-boton5.grid(row=7, column=0)
-boton6= Button(root, text="De M4A - MP3", command = m4a)
-boton6.grid(row=8, column=0)
 
 root.mainloop()
